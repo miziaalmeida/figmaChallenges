5dd1a0474f0c5924a3863640665a503690d872e0
mizialima <mizia.alima@gmail.com>
2022-04-12 19:09:57 -0300

    adiciona pasta desafio 1

---
+++ walkthroughScreen/walkthroughScreen/Scenes/WalkthroughViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  walkthroughScreen
+//
+//  Created by Mizia Lima on 12/04/22.
+//
+
+import UIKit
+
+class WalkthroughViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
